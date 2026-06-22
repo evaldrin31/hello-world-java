78d1bca5e5af0af89eefbb2d1337374f7256ac95
VINO ALDRIN E <192511481.simats@saveetha.com>
2026-06-22 10:04:24 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!");
+        System.out.println("This code was updated by Vino.");
+        System.out.println("Build successful.");
+    }
 }
