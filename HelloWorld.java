4c97897e1e0bdc33a4c471bfdcfc247f29aab61d
VINO ALDRIN E <192511481.simats@saveetha.com>
2026-06-22 10:26:58 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins!");
-        System.out.println("This code was updated by Vino.");
-        System.out.println("Build successful.");
+        System.out.println("=== JENKINS CI/CD DEMO ===");
+        System.out.println("Repository updated successfully!");
+        System.out.println("Build triggered automatically.");
+        System.out.println("Student: Vino Aldrin E");
+        System.out.println("Date: 22-06-2026");
+        System.out.println("Jenkins + GitHub Integration Working!");
     }
 }
